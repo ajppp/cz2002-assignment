362675fb09f3991d001e9db2b4891593b48ca4b2
claudia <61492986+coloteong@users.noreply.github.com>
2020-11-14 14:03:08 +0800

    updated test.java
    i haf pwoblem? ><

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -10,7 +10,18 @@ public class Test{
         Date startTime = new Date(2020, 11, 3, 14, 30);
         Date endTime = new Date(2020, 11, 30, 11, 30);
         LoginPage login = new LoginPage(startTime, endTime);
-        Student cloud = new Student("Claudia", "SCSE", "M", "Singaporean");
+        Student cloud = new Student("Claudia", "SCSE", "M", "Singaporean", "dummyemailforoodp@gmail.com");
+        Student a = new Student("Sarah Lim", "SCSE", "F", "Malaysian", "dummyemailforoodp@gmail.com");
+        Student b = new Student("Hazel Wang", "FASS", "F", "PRC", "dummyemailforoodp@gmail.com");
+        Student c = new Student("Jackson Quek", "MAE", "M", "Singaporean", "dummyemailforoodp@gmail.com");
+        Student d = new Student("Joe Chan", "EEE", "M", "Singaporean", "dummyemailforoodp@gmail.com");
+        Student e = new Student("Emma Miller", "SPMS", "F", "American", "dummyemailforoodp@gmail.com");
+        Student f = new Student("Aaron Lu", "NBS", "M", "Indonesian", "dummyemailforoodp@gmail.com");
+        Student g = new Student("Laura Ng", "ACC", "F", "Singaporean", "dummyemailforoodp@gmail.com");
+        Student h = new Student("Priyaa Singh", "NIE", "F", "Indian", "dummyemailforoodp@gmail.com");
+        Student i = new Student("That Mhu Khin", "CEE", "F", "Burmese", "dummyemailforoodp@gmail.com");
+        Student j = new Student("Brandon Tiong", "REP", "M", "Singaporean", "dummyemailforoodp@gmail.com");
+
         registeredStudentList.add(cloud);
         Index dsai = new Index("SCSE", "1234", 3, 39, 40, lessonList, studentWaitlist, registeredStudentList);
         indices.add(dsai);
@@ -21,7 +32,7 @@ public class Test{
         SerialEditor.writeIndices(indices);
         SerialEditor.writeCourses(courses);
         SerialEditor.writeLogin(login);
-        students.remove(cloud);
+        /* students.remove(cloud);
         indices.remove(dsai);
         courses.remove(oodp);
         students = SerialEditor.loadStudents();
@@ -35,6 +46,6 @@ public class Test{
         }
         for (Index index: indices){
             System.out.println(index.getVacancies());
-        }
+        } */
     }
 }
